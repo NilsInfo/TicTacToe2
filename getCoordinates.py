48e227702066899a6811dd99625e18e8d2cd8b0a
Nils Moynac <e12016308@student.tuwien.ac.at>
2021-10-13 15:48:32 +0200

    add getClick

--- getCoordinates.py
+++ getCoordinates.py
@@ -1,18 +1,30 @@
 import requests
 
-url = 'http://192.168.1.224:8080/qpe/getTagData'
-options = "?mode=json&tag='a4da22e16f5d"
-req = url + options
+url = 'http://192.168.1.224:8080/qpe/getTagData?tag=a4da22e16f5d'
 payload = {}
 headers = {}
+lastTS = 0
+def get():
+    response = requests.get(url, headers=headers, data=payload)
+    print(response.status_code)
 
+    resJson = response.json()
+    return resJson
 
 def getCoordinates():
+    resJson = get()
+    coordinates = resJson["tags"][0]["locationZoneNames"]
+    return coordinates
 
-    response = requests.get(url, headers=headers, data=payload)
-    resJson = response.json()
-    print(response.status_code)
-    coordinates = resJson["tags"][0]["location"]
-    return {coordinates[0],coordinates[1]}
+def getClick():
+    global lastTS
+    resJson = get()
+    print(resJson)
+    tS = resJson["tags"][0]["button1LastPressTS"]
+    if tS > lastTS:
+        lastTS = tS
+        return True
+    else :
+        return False
 
-print(getCoordinates())
+print(getClick())
